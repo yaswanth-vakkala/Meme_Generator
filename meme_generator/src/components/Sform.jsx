b7b64a54a6b4e581802788f237dc196d9d2ae066
yaswanth-vakkala <yaswanthv87@gmail.com>
2022-09-17 18:54:55 +0530

    complete initial functionality

--- meme_generator/src/components/Sform.jsx
+++ meme_generator/src/components/Sform.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sform.css";
+import MemesData from "../MemesData";
 
 export default function Sform() {
+  const [memeImage, setMemeImage] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemesData, setAllMemesData] = useState(MemesData);
+
+  function handleClick() {
+    const url =
+      allMemesData.data.memes[
+        Math.floor(Math.random() * allMemesData.data.memes.length)
+      ].url;
+    setMemeImage((prevImage) => ({
+      ...prevImage,
+      randomImage: url,
+    }));
+  }
   return (
     <div className="sform">
-      <form className="form">
+      <div className="form">
         <input className="tbox1" type="text" placeholder="Top Text"></input>
         <input className="tbox2" type="text" placeholder="Bottom Text"></input>
-        <button className="sbtn" type="submit">
+        <button onClick={handleClick} className="sbtn" type="submit">
           Get a new meme image
         </button>
-      </form>
+      </div>
+      <div className="mImage">
+        <img src={memeImage.randomImage}></img>
+      </div>
     </div>
   );
 }
